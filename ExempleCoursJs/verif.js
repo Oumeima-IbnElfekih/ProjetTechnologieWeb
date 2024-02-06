75d96003c478c3621be8805707b1e5880aa5e4d0
Oumeima-IbnElfekih <oumeima.ibnelfekih@esprit.tn>
2024-02-06 12:16:14 +0100

    semaine3

---
+++ ExempleCoursJs/verif.js
@@ -0,0 +1,35 @@
+function validerDateOfBirth(){
+    var elementDate = 
+    document.getElementById("date_naissance")
+    var elementValue = new Date(elementDate.value) ;
+    const dateAujourdhui= new Date();
+    if(elementValue>=dateAujourdhui)
+    {
+        alert("Please select a correct date that is before today");
+        }
+        else {
+            alert("Date is valid");
+        }
+}
+
+var formElement = document.getElementById("form") 
+var nameElement = document.getElementById("nom");
+formElement.addEventListener("submit", function(event){
+    event.preventDefault()
+    validateName()
+
+}) 
+
+
+function validateName(){
+    var NameValue = nameElement.value ;
+    var pattern = /^[a-z A-Z]+$/
+    var nameError = document.getElementById("NameError")
+    if(!NameValue.match(pattern)){
+          nameError.innerHTML=
+    "<span style='color :red' > Nom ne doit contenir que des lettres et doit avoir au moins 1 caractère </span>"
+    }else {
+        nameError.innerHTML= "<span style='color :green'>Correct</span>"
+    }
+}
+
